2aab5deee50b62061625d57e41b4a9ca99e819be
owlmilo1 <angelomig13@gmail.com>
2022-06-13 09:09:47 -0300

    test remote repository

--- src/testes/HelloWorld.java
+++ src/testes/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World, i am on hotfix branch!!!");
 		System.out.println("This is a test");
+		System.out.println("Another test");
 	}
 	
 }
