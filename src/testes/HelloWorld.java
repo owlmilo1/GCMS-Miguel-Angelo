c2a0aeeeeccf3adad446587574bee75b7bfac89d
owlmilo1 <angelomig13@gmail.com>
2022-06-13 08:58:04 -0300

    commit branch master

--- src/testes/HelloWorld.java
+++ src/testes/HelloWorld.java
@@ -3,7 +3,8 @@ package testes;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!");
+		System.out.println("Hello World, i am not on hotfix branch, im now on the master!!!");
+		System.out.println("This is a test with merge");
 	}
 	
 }
